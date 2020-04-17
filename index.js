855cd8a285e759c098a04328b9d767938b8692ec
Anjola-ogunmefun <ogunmefunanjola@gmail.com>
2020-04-17 19:08:13 +0100

    another commit

--- index.js
+++ index.js
@@ -52,127 +52,12 @@ app.get('/home', auth, (req, res) => {
 
 
 
-//At this point, all todos are displayed
-app.get('/all_todo', auth, (req, res) => {
     
-    TodoModel.find()
-    .then((data) => {
-        console.log('All todos have been fetched', data)
-        return res.status(201).send({
-            code: 201,
-            error: false,
-            message: "Here is a list of todos you have created",
-            data
-        });
-    })
-    .catch((error) => {
-        console.log('There was an error fetching all todos', error)
-        return res.status(500).send({
-            code: 500,
-            error: true,
-            message: "Internal server error",
-        });
-    })
-    
-})
-
-//This point returns a particular todo by id
-app.put('/one_todo', auth, (req, res) => {
-    TodoModel.findOne({id: req.body.id})
-    .then((data) => {
-        console.log("Todo has been found" , data)
-        return res.status(201).send({
-            code: 201,
-            error: false,
-            message: "Here is the todo you requested to find",
-            data
-        });
-    })
-    .catch((error) => {
-        console.log('There was an error fetching todo', error)
-        return res.status(500).send({
-            code: 500,
-            error: true,
-            message: "Internal server error",
-        });
-    })
-
-})
-
-//An extension of one_todo witch doesnt just get but also updates
-app.patch('/update', auth, (req, res,) => {
-   
-         const completed = req.body.completed;
-         const title = req.body.title;
-         const description = req.body.description;
-
-       let params ={};
-
-       if(completed){
-           params.completed = completed
-       }
-         if(title){
-            params.title = title
-         }
-         if(description){
-             params.description = description
-         }
 
 
-
-     TodoModel.findOneAndUpdate({id: req.body.id}, params, {new: true} )
-        .then((data) => {
-             console.log("Todo has been updated" , data)
-            return res.status(200).send({
-             code: 200,
-             error: false,
-             message: "Todo updated successful!",
-             data
-             });
-            
-        })
-            .catch((error) => {
-                console.log('There was an error updating todo', error)
-                return res.status(500).send({
-                    code: 500,
-                    error: true,
-                    message: "Internal server error",
-                });
-            })
-        })
         
 
-        // This guy right here deletes a todo from the database
-        app.delete('/delete', auth, (req, res) => {
-            if({id: req.body.id == false}){
-                console.log('Todo is missing');
-                return res.status(205).send({
-                    code: 205,
-                    error: true,
-                    message: "Todo does not exist!"
-                })
-            };
-
-            TodoModel.findOneAndDelete({id: req.body.id})
-            .then((data) => {
-                console.log("Todo has been deleted" , data)
-               return res.status(200).send({
-                code: 200,
-                error: false,
-                message: "Todo has been deleted successfully!",
-                data
-                });
-           })
-
-           .catch((error) => {
-            console.log('There was an error deleting todo', error)
-            return res.status(500).send({
-                code: 500,
-                error: true,
-                message: "Internal server error",
-            });
-        })
-        })
+        
 
 
 
--- routes/todo.js
+++ routes/todo.js
@@ -59,4 +59,152 @@ todoRoute.post('/create-todo', auth, (req, res) => {
     
 });
 
+//At this point, all todos are displayed
+todoRoute.get('/all-todo', auth, (req, res) => {
+    
+    TodoModel.find()
+    .then((data) => {
+        console.log('All todos have been fetched', data)
+        return res.status(201).send({
+            code: 201,
+            error: false,
+            message: "Here is a list of todos you have created",
+            data
+        });
+    })
+    .catch((error) => {
+        console.log('There was an error fetching all todos', error)
+        return res.status(500).send({
+            code: 500,
+            error: true,
+            message: "Internal server error",
+        });
+    })
+    
+})
+
+
+//This point returns a particular todo by id
+todoRoute.get('/one-todo', auth, (req, res) => {
+    const id = req.body.id
+    if(!id){
+        return res.status(400).send({
+            code: 400,
+            error: true,
+            message: "Kindly add id to the request body"
+        })
+    }
+
+    TodoModel.findOne({id: req.body.id})
+    .then((data) => {
+        console.log("Todo has been found" , data)
+        return res.status(201).send({
+            code: 201,
+            error: false,
+            message: "Here is the todo you requested to find",
+            data
+        });
+    })
+    .catch((error) => {
+        console.log('There was an error fetching todo', error)
+        return res.status(500).send({
+            code: 500,
+            error: true,
+            message: "Internal server error",
+        });
+    })
+
+})
+
+
+//An extension of one_todo witch doesnt just get but also updates
+todoRoute.patch('/update', auth, (req, res,) => {
+   
+    const completed = req.body.completed;
+    const title = req.body.title;
+    const description = req.body.description;
+    const id = req.body.id
+
+  let params ={};
+
+  if(completed){
+      params.completed = completed
+  }
+    if(title){
+       params.title = title
+    }
+    if(description){
+        params.description = description
+    }
+
+    if(!id){
+       return res.status(400).send({
+           code: 400,
+           error: true,
+           message: "Kindly add id to the request body"
+       })
+   }
+
+    // if(!params){
+    //    return res.status(400).send({
+    //         code: 400,
+    //         error:true,
+    //         message:"kindly input an update"
+    //     })
+    // }
+
+TodoModel.findOneAndUpdate({id:req.body.id}, params, {new: true} )
+   .then((data) => {
+        console.log("Todo has been updated" , data)
+       return res.status(200).send({
+        code: 200,
+        error: false,
+        message: "Todo updated successful!",
+        data
+        });
+       
+   })
+       .catch((error) => {
+           console.log('There was an error updating todo', error)
+           return res.status(500).send({
+               code: 500,
+               error: true,
+               message: "Internal server error",
+           });
+       })
+    
+    })
+// This guy right here deletes a todo from the database
+    todoRoute.delete('/delete-one', auth, (req, res) => {
+    if(!{id: req.body.id}){
+        console.log('Todo is missing');
+        return res.status(205).send({
+            code: 205,
+            error: true,
+            message: "Todo does not exist!"
+        })
+    };
+
+    TodoModel.findOneAndDelete({id: req.body.id})
+    .then((data) => {
+        console.log("Todo has been deleted" , data)
+       return res.status(200).send({
+        code: 200,
+        error: false,
+        message: "Todo has been deleted successfully!",
+        data
+        });
+   })
+
+   .catch((error) => {
+    console.log('There was an error deleting todo', error)
+    return res.status(500).send({
+        code: 500,
+        error: true,
+        message: "Internal server error",
+    });
+})
+})
+
+
 module.exports = todoRoute;
